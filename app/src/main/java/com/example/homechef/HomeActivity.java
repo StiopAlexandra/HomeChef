a510a46e9915947c1ea2a99710faa083b806e78f
Stiop Alexandra <stiop.alexandra@yahoo.com>
2022-05-15 23:58:10 +0300

    feat: add loging/register pages and menu

---
+++ app/src/main/java/com/example/homechef/HomeActivity.java
@@ -0,0 +1,54 @@
+package com.example.homechef;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class HomeActivity extends AppCompatActivity{
+
+    BottomNavigationView bottomNavigationView;
+    HomeFragment homeFragment = new HomeFragment();
+    ShoppingListFragment shoppingListFragment = new ShoppingListFragment();
+    FavoritesFragment favoritesFragment = new FavoritesFragment();
+    SettingsFragment settingsFragment = new SettingsFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+                        return true;
+                    case R.id.shoppingList:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, shoppingListFragment).commit();
+                        return true;
+                    case R.id.favorites:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, favoritesFragment).commit();
+                        return true;
+                    case R.id.settings:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, settingsFragment).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/homechef/User.java
@@ -0,0 +1,10 @@
+package com.example.homechef;
+
+public class User {
+    public String fullName, email;
+
+    public User(String fullName, String email){
+        this.fullName = fullName;
+        this.email = email;
+    }
+}
